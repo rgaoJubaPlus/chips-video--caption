6767e91238b233fffe8408c52e6a84c1db080118
rgaoJubaPlus <44410786+rgaoJubaPlus@users.noreply.github.com>
2022-12-22 14:22:42 -0500

    delete the slash for endpoint

--- src/app/https.service.ts
+++ src/app/https.service.ts
@@ -12,7 +12,7 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class HttpsService {
-  private generateURL = 'http://localhost:8000/static_insertion/';
+  private generateURL = 'http://localhost:8000/static_insertion';
   constructor(private http: HttpClient) {}
   generateVideo(body: any) {
     return this.http.post<any>(this.generateURL, body, httpOptions);
